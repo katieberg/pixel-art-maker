a27fe9c718b7617086f7a191b46f8f4486b7eacb
unknown <kberg1@email.arizona.edu>
2019-05-09 19:19:29 -0700

    fixed spacing between rows and made squares smaller

--- Pixel.js
+++ Pixel.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     var brushColor="pink"
     var paintZone = document.querySelector("#pixelPainter")
-    var paintSize = 20
+    var paintSize = 40
     for(let i=0;i<paintSize;i++){
-        var newRow=document.createElement("div");//something below this breaks it
+        var newRow=document.createElement("div");
+        newRow.style.height="14px";
         paintZone.appendChild(newRow);
-
         for(let j=0;j<paintSize;j++){
             var newCell=document.createElement("div");
             newCell.classList.add("square");
             newRow.appendChild(newCell);
-        }//smething above this breaks it
+        }
     }
     var paint = function(event){
         if(event.target.classList.contains("square")){
