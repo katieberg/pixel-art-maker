59031e33598558032fab187f7c8d263b10cb7be2
unknown <kberg1@email.arizona.edu>
2019-05-09 17:10:18 -0700

    event listener when paint swatch is clicked so the paintbrush changes colors. functions but need to examine the event listeners

--- Pixel.js
+++ Pixel.js
@@ -1,8 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
+    var brushColor="pink"
     var paint = function(event){
         if(event.target.classList.contains("square")){
-            event.target.style.backgroundColor="green"
+            event.target.style.backgroundColor=brushColor;
         }
     }
     window.addEventListener("click",paint);
+
+    var switchBrush = function(event){
+        if(event.target.classList.contains("circle")){
+            brushColor=event.target.style.backgroundColor
+        }
+    }
+    window.addEventListener("click",switchBrush);
 })
