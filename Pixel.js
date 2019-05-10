1b394335f1c8a310c977ad46300ae4264be1c4ff
unknown <kberg1@email.arizona.edu>
2019-05-09 17:27:12 -0700

    expanded dimensions of the paintable space and added some paint color options

--- Pixel.js
+++ Pixel.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     var brushColor="pink"
+    var paintZone = document.querySelector("#pixelPainter")
+    var paintSize = 20
+    for(let i=0;i<paintSize;i++){
+        var newRow=document.createElement("div");//something below this breaks it
+        paintZone.appendChild(newRow);
+
+        for(let j=0;j<paintSize;j++){
+            var newCell=document.createElement("div");
+            newCell.classList.add("square");
+            newRow.appendChild(newCell);
+        }//smething above this breaks it
+    }
     var paint = function(event){
         if(event.target.classList.contains("square")){
             event.target.style.backgroundColor=brushColor;
